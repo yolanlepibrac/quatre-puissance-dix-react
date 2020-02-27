fd3e73802d9e10a3c9e15b85032575dffda10d19
yolanlepibrac <yolan.pibrac@gmail.comleavekjcjknvsdvgit config --global user.email yolan.pibrac@gmail.com>
2020-02-27 22:05:31 +0100

    merge

--- src/components/API.js
+++ src/components/API.js
@@ -6,11 +6,32 @@ const headers = {
   "Access-Control-Allow-Origin": "Origin"
 };
 
-const burl = "http://localhost:7000";
+const burl = "https://test-mongoose-qpd.herokuapp.com";
 
 module.exports = {
   login: function(email, password) {
-    return axios.post(burl + "/users", { email: email, password: password }, { headers: headers });
+    console.log(email);
+    return axios.post(
+      burl + "/user/login",
+      { email: email, password: password },
+      { headers: headers }
+    );
+    /* var xhttp = new XMLHttpRequest();
+    try {
+      xhttp.onreadystatechange = function() {
+        console.log(xhttp);
+        if (xhttp.readyState == 4 && xhttp.status == 0) {
+          alert("Unknown Error Occured. Server response not received.");
+        }
+      };
+      xhttp.open("POST", "http://localhost:7000/users", true);
+      xhttp.send();
+    } catch (e) {
+      console.log("catch", e);
+    }
+    return new Promise((resolve, res) => {
+      resolve();
+    }); */
   },
   register: function(email, username, password) {
     return axios.post(
