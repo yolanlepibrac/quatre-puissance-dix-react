cd139495f29b3ef1b1fafe180028c7e669ba5f30
yolan <yolan.pibrac@gmail.com>
2020-03-16 21:43:30 +0100

    yellow color of player 2

--- src/components/Constantes.js
+++ src/components/Constantes.js
@@ -4,7 +4,7 @@ export default {
   colorText1: "rgba(65, 181, 127, 1)",
   colorText3: "rgba(173, 25, 17,1)",
   colorPlayer1: "rgb(173, 25, 17)",
-  colorPlayer2: "rgb(219, 204, 35)",
+  colorPlayer2: "rgb(250, 255, 94)",
   colorAxe1: { color1: "rgba(173, 25, 17,1)", color2: "rgba(153, 10, 10,1)" },
   colorAxe2: { color1: "rgb(84, 224, 159)", color2: "rgba(65, 181, 127, 1)" },
   colorAxe3: { color1: "rgba(0, 78, 204,1)", color2: "rgba(0, 48, 220,1)" },
